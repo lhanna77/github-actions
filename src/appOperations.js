50e7683da06db2a43aaaea4bf49506f8592696e2
lhanna77 <98017236+lhanna77@users.noreply.github.com>
2026-09-08 11:49:23 +0100

    Update appOperations.js

--- src/appOperations.js
+++ src/appOperations.js
@@ -2,4 +2,11 @@ function multiply(num1, num2) {
     return num1 * num2;
 }
 
-module.exports = multiply
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+module.exports = {
+    multiply,
+    add
+}
